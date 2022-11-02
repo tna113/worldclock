e99a095edbc9f9185c32df6842aac95e0e3ffdd0
ta7384 <ta7384@g.rit.edu>
2022-11-01 20:19:29 -0400

    Merge branch 'main' of https://github.com/tna113/worldclock

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const imgBasePath = './assets/globe';
+var imgIndex = 0;
+
 function getTime() {
     const today = new Date();
     // console.log('today:' + today);
@@ -5,36 +8,47 @@ function getTime() {
     let h = today.getHours(); //0-23
     let m = today.getMinutes(); //0-59
     let s = today.getSeconds(); //0-59
+    // console.log('h:'+h+' m:'+m+' s:'+s);
 
     var time = document.getElementById('time');
     // time.textContent = h + ':' + m + ':' + s;
     time.textContent = getTimeString([h,m,s]);
-    setTimeout(getTime,500);
+    changeImg();
+    setTimeout(getTime,1000);
 }
 
 function format(type,i) {
+
     switch(type) {
         case 'h': //0-23
             if(i>12) {
+                // console.log(i+"-12="+(i-12));
                 //if i>12, minus 12 from i ie. 13 -12 = 1 
                 i = i-12;
 
-                //then format again if number less than 10
                 if (i<10) {
-                    format(type,i);
+                    i = addZero(i);
                 }
-
-                break;
             }
+            break;
         case 'm' || 's': //0-59
             if(i<10) {
                 //if i<10, add 0 in front ie. 2 -> 02
-                i = "0" + i;    
+                i = addZero(i);
             }
             break;
-        default:   console.log('Could not format time');
+        default: 
+            // weird bug here shows on console
+            // console.log('Could not format time on type: '+type+" and i: "+i);
             break;
     }
+
+
+    function addZero(num) {
+        return "0"+num;
+    }
+
+    // console.log('format('+type+','+i+') result i: '+i);
     return i;
 }
 
@@ -42,8 +56,8 @@ function getTimeString(arr) {
     let types = ['h','m','s'];
     let str = "";
 
-    console.log(types);
-    console.log(types[0] + "," + arr[0]);
+    // console.log('getTimeString() types:' + types);
+    // console.log('getTimeString() types[0]: ' + types[0] + ", arr[0]: " + arr[0]);
 
     // console.log('arr.length: '+arr.length); //3
     for(let len = arr.length, i = 0; i < len; i++) {
@@ -52,5 +66,22 @@ function getTimeString(arr) {
 
     //redact last character ":"
     str = str.substring(0,str.length-1);
+
+
+    // console.log('getTimeString('+arr+') result: '+str);
+
     return str;
 } 
+
+function changeImg() {
+    console.log("changeImg() start imgIndex:"+imgIndex);
+
+    if (imgIndex==4) {
+        imgIndex = 0;
+    }
+
+    document.getElementById('globe').setAttribute("src",imgBasePath+(imgIndex+1)+".png");
+    imgIndex++;
+
+    console.log("changeImg() end imgIndex:"+imgIndex);
+}
